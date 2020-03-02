0d14d232778dba6ba5e9313c3c747415671192cd
Adarsh Dharmadevan <adarshdharmadevan@gmail.com>
2020-03-02 04:30:01 +0000

    Update index.js

--- inhobi/src/index.js
+++ inhobi/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import { MastertaskPage } from './figmaComponents';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<MastertaskPage />, document.getElementById('root'));
