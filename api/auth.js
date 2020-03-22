da79fef8433a00067e43238faeabd942a22e63c3
Arjun Andra <arjun.andra@gmail.com>
2020-03-22 10:42:39 +0530

    /login Route Fixed

--- api/auth.js
+++ api/auth.js
@@ -1,20 +1,20 @@
 var express = require('express');
+const cors = require('cors');
 var router = express.Router();
 const axios = require('axios');
 
 
 router.get('/login', function(req, res, next){
-    console.log(req.body["username"]);
+    
     var token = '';
-    const args = {
-            username : req.body["username"],
-            password : req.body["password"],
-            service : "moodle_mobile_app"
-    }
-    axios.post('http://68.183.95.58/moodle/login/token.php', args).then(function(response){
-        console.log(response);
-    });
-    res.send("hello!")
+    const args = {}
+
+    axios.post(`http://68.183.95.58/moodle/login/token.php?username=${req.body.username}&password=${req.body.password}&service=moodle_mobile_app`, args).then(function(response){
+        token = response.data.token;
+        res.send(`Logged In With Token: ${token}`)
+    }).catch((err) => console.log(err));
+
+    
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
 const auth = require('./api/auth');
-const bodyParser = require('body-parser');
+const cors = require('cors');
 
-app.use(bodyParser.urlencoded({
-    extended: true
-  }));
-app.use(bodyParser.json())
+const PORT = 8080
+
+app.use(cors())
+app.use(express.json())
+
 app.use('/', auth)
-app.listen(8080)
+app.listen(PORT, () => {
+  console.log("Server Is Running On Port: " + PORT)
+})
