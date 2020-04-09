a20df4c34970740f0c08ffe1d870eaf72785ef11
adarsh <adarsh@pop-os.localdomain>
2020-04-09 13:17:01 +0530

    fixed tasksubmission styling errors

--- src/pages/taskSubmission.js
+++ src/pages/taskSubmission.js
@@ -50,7 +50,7 @@ class Sub extends Component {
                     </div>
                     <div class="upload">
                         <div>
-                            <svg xmlns="http://www.w3.org/2000/svg"  height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M5 4v2h14V4H5zm0 10h4v6h6v-6h4l-7-7-7 7z"/></svg>
+                            <svg xmlns="http://www.w3.org/2000/svg"  height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path fill="white" d="M5 4v2h14V4H5zm0 10h4v6h6v-6h4l-7-7-7 7z"/></svg>
                         </div>
                         <div>
                             Upload
