42cea9efccc341743903bc59e5f1fb038cb8ee77
adarsh <adarsh@pop-os.localdomain>
2020-04-09 11:42:06 +0530

    updates cmdashboard to include namespace for all css

--- src/pages/CmDashboardPage.js
+++ src/pages/CmDashboardPage.js
@@ -36,10 +36,10 @@ export default class CmDashboardPage extends Component {
     }
     render() {
         return (
-            <div>
+            <div className="CmDashboardPage">
                 <div className="top_bars" >
                     <TitleBar title="Dashboard" />
-                    <TabBar tabs={{ "Tasks": true, "Group": false }} click={[this.clickTasks, this.clickGroup]} />/>
+                    <TabBar tabs={{ "Tasks": true, "Group": false }} click={[this.clickTasks, this.clickGroup]} />
                 </div>
                 <div className="container">
                     <TaskCard task_name="Task 3" user_name="jeff" time_submited="21:15:00" />
