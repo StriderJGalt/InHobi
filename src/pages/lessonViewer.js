915038166b6bd69409ad4d53a249a12606f242c3
Arjun Andra <arjun.andra@gmail.com>
2020-04-10 11:17:14 +0530

    Fixed react-player

--- src/pages/lessonViewer.js
+++ src/pages/lessonViewer.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import ReactAwesomePlayer from 'react-awesome-player';
+import ReactPlayer from 'react-player';
 import '../css/lessonViewer.css';
 import { Table, TableRow, TableCell } from '@material-ui/core';
 import { TitleBar, TabBar, BottomNavBar } from '../Bars.js'
@@ -45,24 +45,13 @@ export default class MasterlessonViewer extends Component {
       <TitleBar title="Lesson" class="titlebar" />
 
       <div class="inner">
-      <ReactPlayer />
+      <ReactPlayerComp />
 
       <div class="playerTitle">
           <b>Now Playing:</b> Lesson # - Lesson Description
       </div>
 
       <TabBar tabs={{"Lesson Plan": this.state.lessonPlan, "Group": this.state.group}} click={[this.clickLessonPlan, this.clickGroup]} class="tabbar" />
-        
-     
-      {/* <div class="lessonButtonDiv">
-        <div class="lessonButton">
-          <button> Lesson Plan </button>
-        </div>
-          
-        <div class="lessonButton">
-          <button> Group </button>
-        </div>
-      </div> */}
 
       <Table class="lessonTable">
         <LessonRow lessonNumber="Lesson 1" lessonName="Test Lesson" time="10 Mins" click={this.click}/>
@@ -75,78 +64,22 @@ export default class MasterlessonViewer extends Component {
   }
 }
 
-class ReactPlayer extends Component {
+class ReactPlayerComp extends Component {
 
   constructor(props) {
     super(props)
   }
 
-  state = {
-    options: {
-      poster: "http://pic2.52pk.com/files/130514/1283314_143556_2145.jpg",
-      sources: [{
-        type: "video/mp4",
-        src: "https://cdn.theguardian.tv/webM/2015/07/20/150716YesMen_synd_768k_vp8.webm"
-      }],
-      subtitles: [{
-          language: 'zh',
-          url: "https://peng666.github.io/react-awesome-player/zh.vtt",
-          label: "中文"
-        },
-        {
-          language: 'en',
-          url: "https://peng666.github.io/react-awesome-player/en.vtt",
-          label: "EN"
-      }],
-      defaultSubtitle: 'en'
-    }
-  }
-  loadeddata() {
-    console.log('loadeddata')
-  }
-  canplay() {
-    console.log('canplay')
-  }
-  canplaythrough() {
-    console.log('canplaythrough')
-  }
-  play() {
-    console.log('play')
-  }
-  pause() {
-    console.log('pause')
-  }
-  waiting() {
-    console.log('waiting')
-  }
-  playing() {
-    console.log('playing')
-  }
-  ended() {
-    console.log('ended')
-  }
-  error() {
-    console.log('error')
-  }
-
   render () {
-    const { options } = this.state
 
     return (
-      <div class="player">
-        <ReactAwesomePlayer
-          options={options}
-          loadeddata={this.loadeddata}
-          canplay={this.canplay}
-          canplaythrough={this.canplaythrough}
-          play={this.play}
-          pause={this.pause}
-          waiting={this.waiting}
-          playing={this.playing}
-          ended={this.ended}
-          error={this.error}
+      
+        <ReactPlayer
+          url="https://www.youtube.com/watch?v=AuW-7YGkb0g"
+          width="100%"
+          class="player"
         />
-      </div>
+      
     )
   }
 }
