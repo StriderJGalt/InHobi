a067113832f51c900490bd0c2db1ec276aae5b50
RazonJoseph <razonjoseph123@gmail.com>
2020-04-15 18:50:04 +0530

    Added instructor_profile_courses.js

---
+++ src/pages/instructor_profile_courses.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react'
+import '../css/instructor_profile_courses.css'
+import course_icon from '../css/camera_web.jpg'
+import { TitleBar, TabBar, BottomNavBar } from '../Bars'
+import axios from "axios";
+
+export class Instructor_courses extends Component {
+    constructor(props) {
+        super(props)
+
+        this.clickAbout = this.clickAbout.bind(this)
+        this.clickCourses = this.clickCourses.bind(this)
+        this.clickFeed = this.clickFeed.bind(this)
+    }
+
+    clickAbout() {
+        alert("About")
+    }
+
+    clickFeed() {
+        alert("Feed")
+    }
+
+    clickCourses() {
+        this.props.history.push({
+            pathname: '/ins_courses',
+        });
+    }
+
+    componentDidMount() {
+        axios.post('http://localhost:8080/course/courses')
+    }
+    render() {
+        console.log('render')
+        return (
+            <div className="Instructor_courses">
+                <div className="top_bars" >
+                    <TitleBar title="Instructor Profile" />
+                    <TabBar tabs={{ "About": false, "Courses": true, "Feed": false }} click={[this.clickAbout, this.clickCourses, this.clickFeed]} />
+                </div>
+                <div className='courses container'>
+                    <Course_prof img={course_icon} course_name='Introduction to Photography' no_of_lessons='21 Lessons' />
+                    <Course_prof img={course_icon} course_name='Machine Learning' no_of_lessons='15 Lessons' />
+
+                </div>
+                <BottomNavBar />
+            </div>
+        )
+    }
+}
+
+class Course_prof extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        }
+    };
+    render() {
+        return (
+            <div className='course'>
+                <div className='course-icon' >
+                    <img src={this.props.img} />
+                </div>
+                <div className='course-name'>
+                    {this.props.course_name}
+                </div>
+                <div className='no_of_lessons'>
+                    {this.state.progress}
+                </div>
+            </div>
+        )
+    }
+}
+
